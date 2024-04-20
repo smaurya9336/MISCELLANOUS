23c74d82946cd55b02383ac2413325352ca815f5
Sarita <sm7767119@gmail.com>
2024-04-20 22:43:35 +0530

    Get and Post method

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/register", (req, res) => {
+  let { user, password } = req.query;
+  res.send(`standard GET response. Welcome ${user}`);
+});
+
+app.post("/register", (req, res) => {
+  let { username, password } = req.body;
+  // console.log(req.body);
+  res.send(`standard Post response. Welcome ${username}`);
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
